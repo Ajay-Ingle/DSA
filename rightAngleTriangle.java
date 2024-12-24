c40c65494d42369695fb5655d95994a0c4d4075b
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-24 23:06:51 +0530

    Added changes

---
+++ rightAngleTriangle.java
@@ -0,0 +1,12 @@
+// package PatternsInJava;
+
+public class rightAngleTriangle {
+    public static void main(String[] args) {
+        for(int i=0; i<=4; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
