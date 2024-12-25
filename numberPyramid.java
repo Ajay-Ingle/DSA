26560ebdf0e893eb9387c0641ac99675a3a0a78a
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2024-12-25 17:33:52 +0530

    Added Number pyramid

---
+++ numberPyramid.java
@@ -0,0 +1,15 @@
+public class numberPyramid {
+    public static void main(String[] args) {
+        int num =5;
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=(num-i); j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
