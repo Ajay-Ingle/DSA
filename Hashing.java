c8e89a811b67d18f17a0287840f419841ef1b5bd
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2025-01-06 23:08:40 +0530

    creating hashset

--- Hashing.java
+++ Hashing.java
@@ -2,6 +2,25 @@ import java.util.HashSet;
 
 public class Hashing {
     public static void main(String[] args) {
+        HashSet <Integer> set = new HashSet<>();
+
+        //insert - using add() fun
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(1);
+
+        //Search - contains() func
+        if(set.contains(1)){
+            System.out.println("Set contains 1");
+        }
+        if(!set.contains(9)){
+            System.out.println("The set doesnot contains 9");
+        }
         
+        
+        
+
     }
 }
