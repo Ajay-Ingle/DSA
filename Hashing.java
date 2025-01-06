8c9b82514fc6d4da5e7f864560ad3d2567e84de1
Ajay-Ingle <ingleajaybhimrao@gmail.com>
2025-01-06 23:26:39 +0530

    Added HashSet basic concepts

--- Hashing.java
+++ Hashing.java
@@ -1,5 +1,5 @@
 import java.util.HashSet;
-
+import java.util.Iterator;
 public class Hashing {
     public static void main(String[] args) {
         HashSet <Integer> set = new HashSet<>();
@@ -11,6 +11,10 @@ public class Hashing {
         set.add(4);
         set.add(1);
 
+        //set size
+        System.out.println("the size of set is: "+set.size());
+        System.out.println("the elements of set is: "+set);
+
         //Search - contains() func
         if(set.contains(1)){
             System.out.println("Set contains 1");
@@ -19,8 +23,18 @@ public class Hashing {
             System.out.println("The set doesnot contains 9");
         }
         
+        // set.remove(1);
+        // if(set.contains(1)){
+        //     System.out.println("Set contains 1");
+        // }else{
+        //     System.out.println("1 is removed");
+        // }
         
-        
-
+        //Iterator
+        Iterator it = set.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+            //doest not requires iteration it automatically get iterated
+        }
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+   // set.remove(1);
+        // if(set.contains(1)){
+        //     System.out.println("Set contains 1");
+        // }else{
+        //     System.out.println("1 is removed");
+        // }
